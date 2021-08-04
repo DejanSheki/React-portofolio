e50f15fe05abac453206ae29e0ea8ff7a449a006
Dejan <dejan.sheki.lukic@gmail.com>
2021-08-04 14:52:28 +0200

    Styled Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+// Global style
+import GlobalStyle from "./components/GlobalStyle";
 // Import pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
