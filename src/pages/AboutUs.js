4e106a5a565e555eb2a18c859f1fe032af7fbd22
Dejan <dejan.sheki.lukic@gmail.com>
2021-08-04 12:16:47 +0200

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,15 @@
 import React from 'react';
 // Page components
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const AboutUs = () => {
     return (
-        <AboutSection />
-    )
+        <>
+            <AboutSection />
+            <ServicesSection />
+        </>
+    );
 }
 
 export default AboutUs;
