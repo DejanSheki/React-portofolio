9a307733407b8ec9c624f9d409946d068929e336
Dejan <dejan.sheki.lukic@gmail.com>
2021-08-05 10:17:34 +0200

    React Router DOM

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 // Import pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+// Router
+import { Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
