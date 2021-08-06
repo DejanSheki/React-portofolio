0c7c386f234bc8454b5d8a43ffb60543139e84c1
Dejan <dejan.sheki.lukic@gmail.com>
2021-08-06 15:03:24 +0200

    Framer motion animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import home1 from '../img/home1.png';
+import Wave from './Wave.js';
 // import styled from 'styled-components';
 import { About, Description, Image, Hide } from '../styles';
 // Framer Motion
 import { motion } from 'framer-motion';
+import { titleAnimation, fade, photoAnimation } from '../animation';
 
 const AboutSection = () => {
     return (
@@ -11,22 +13,23 @@ const AboutSection = () => {
             <Description>
                 <motion.div className="title">
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnimation}>
                             We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>your <span>dreams</span> come</motion.h2>
+                        <motion.h2 variants={titleAnimation}>your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>true.</motion.h2>
+                        <motion.h2 variants={titleAnimation}>true.</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
-                <button>Contact Us</button>
+                <motion.p variants={fade}>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="guy with camera" />
+                <motion.img variants={photoAnimation} src={home1} alt="guy with camera" />
             </Image>
+            <Wave />
         </About>
     )
 }
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -7,7 +7,7 @@ import theracer from '../img/theracer-small.png';
 import goodtimes from '../img/goodtimes-small.png';
 // Animations
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import { pageAnimation, fade, photoAnimation, lineAnimation, slider, sliderContainer } from '../animation';
 
 const OurWork = () => {
     return (
@@ -18,11 +18,19 @@ const OurWork = () => {
             exit="exit"
             style={{ background: "#fff" }}
         >
+            <motion.div variants={sliderContainer}>
+                <Frame1 variants={slider}></Frame1>
+                <Frame2 variants={slider}></Frame2>
+                <Frame3 variants={slider}></Frame3>
+                <Frame4 variants={slider}></Frame4>
+            </motion.div>
             <Movie>
-                <h2>The Athlete</h2>
-                <div className="line"></div>
+                <motion.h2 variants={fade}>The Athlete</motion.h2>
+                <motion.div variants={lineAnimation} className="line"></motion.div>
                 <Link to="/work/the-athlete">
-                    <img src={athlete} alt="The Athlete" />
+                    <Hide>
+                        <motion.img variants={photoAnimation} src={athlete} alt="The Athlete" />
+                    </Hide>
                 </Link>
             </Movie>
             <Movie>
@@ -55,7 +63,7 @@ const Movie = styled.div`
     padding-bottom: 10rem;
     .line {
         height: 0.5rem;
-        background: #cccccc;
+        background: #23d997;
         margin-bottom: 3rem;
     }
     img {
@@ -63,6 +71,29 @@ const Movie = styled.div`
         height: 70vh;
         object-fit: cover;
     }
+`;
+const Hide = styled.div`
+    overflow: hidden;
+`;
+
+// Frame Animation
+const Frame1 = styled(motion.div)`
+    position: fixed;
+    left: 0;
+    top: 10%;
+    width: 100%;
+    height: 100vh;
+    background: #fffebf;
+    z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+    background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+    background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+    background: #8effa0;
 `;
 
 export default OurWork;
